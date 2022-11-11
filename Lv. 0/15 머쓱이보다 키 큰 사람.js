83d529cec0818ca7bbf1ff0946049cce64b944cc
takyoung618 <wisdom8751@gmail.com>
2022-11-11 16:00:47 +0900

    15 머쓱이보다 키 큰 사람

---
+++ Lv. 0/15 머쓱이보다 키 큰 사람.js
@@ -0,0 +1,15 @@
+// 머쓱이보다 키 큰 사람
+// 문제 설명
+// 머쓱이는 학교에서 키 순으로 줄을 설 때 몇 번째로 서야 하는지 궁금해졌습니다. 머쓱이네 반 친구들의 키가 담긴 정수 배열 array와 머쓱이의 키 height가 매개변수로 주어질 때, 머쓱이보다 키 큰 사람 수를 return 하도록 solution 함수를 완성해보세요.
+
+// 제한사항
+// 1 ≤ array의 길이 ≤ 100
+// 1 ≤ height ≤ 200
+// 1 ≤ array의 원소 ≤ 200
+
+// filter는 특정 조건을 만족하는 새로운 배열을 필요로 할 때 사용.
+function solution(array, height) {
+    return array.filter(a => a > height).length
+}
+
+
