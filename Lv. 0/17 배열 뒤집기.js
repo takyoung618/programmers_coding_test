4465e24dfc907168ed7e6597dcf96d744a076afa
takyoung618 <wisdom8751@gmail.com>
2022-11-14 15:12:00 +0900

    16 피자 나눠 먹기(1)

---
+++ Lv. 0/17 배열 뒤집기.js
@@ -0,0 +1,14 @@
+// 배열 뒤집기
+//  네트워크 연결 끊김
+// 문제 설명
+// 정수가 들어 있는 배열 num_list가 매개변수로 주어집니다. num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 1 ≤ num_list의 길이 ≤ 1,000
+// 0 ≤ num_list의 원소 ≤ 1,000
+
+// reverse() 라는 배열의 순서를 뒤집는 메소드를 사용
+function solution(num_list) {
+    let answer = num_list.reverse();
+    return answer;
+}
