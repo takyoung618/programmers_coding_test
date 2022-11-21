f338510f0665121fa44fa54ab44e255400d97cbe
takyoung618 <wisdom8751@gmail.com>
2022-11-21 20:10:47 +0900

    30 순서쌍의 개수

---
+++ Lv. 0/30 순서쌍의 개수.js
@@ -0,0 +1,27 @@
+// 순서쌍의 개수
+// 문제 설명
+// 순서쌍이란 두 개의 숫자를 순서를 정하여 짝지어 나타낸 쌍으로 (a, b)로 표기합니다. 자연수 n이 매개변수로 주어질 때 두 숫자의 곱이 n인 자연수 순서쌍의 개수를 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 1 ≤ n ≤ 1,000,000
+
+// 내가 푼 풀이
+function solution(n) {
+    let answer = 0;
+  	let nums = [];
+  	for(let i=1; i<=n; i++){
+      if(n%i === 0) nums = [...nums, i]
+    }
+    return nums.length
+}
+
+// 다른 사람이 푼 풀이
+function solution(n) {
+    let count = 0;
+    for (let i = 1; i < Math.sqrt(n); i++) {
+      if (n % i === 0) count += 2;
+    }
+    if (n % Math.sqrt(n) === 0) count += 1;
+    return count;
+}
+
