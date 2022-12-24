b507f9a74eed5a7b45a17d4e33f0a3f9c703abaf
takyoung618 <wisdom8751@gmail.com>
2022-12-24 17:40:00 +0900

    1-9 같은 숫자는 싫어

---
+++ Lv. 1/09 같은 숫자는 싫어.js
@@ -0,0 +1,11 @@
+// 09 같은 숫자는 싫어
+
+function solution(arr){
+    let answer = [];
+    for(let i =0; i<arr.length; i++){
+      if(arr[i] !== arr[i+1]){
+        answer.push(arr[i])
+      }
+    }
+    return answer
+}
